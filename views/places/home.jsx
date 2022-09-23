574aecbc99557a9db2322ee5704cdd818e298cbb
Matthew Perrot <Matthew@Matthews-MacBook-Air.local>
2022-09-22 21:30:15 -0700

    Added navbar

--- views/places/home.jsx
+++ views/places/home.jsx
@@ -3,17 +3,21 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
+        <Def>
+            <main>
+              <h1>REST-Rant</h1>
+              <div>
+                <img src="https://images.unsplash.com/photo-1490474504059-bf2db5ab2348?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Chia Fruit Shake" />
+                <div>
+                  Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                </div>
+              </div>
+              <a href="/places">
+                  <button className="btn-primary">Places Page</button>
+              </a>
           </main>
-      </Def>
+        </Def>
     )
-  }  
-
-<a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
-
+}
 
 module.exports = home
