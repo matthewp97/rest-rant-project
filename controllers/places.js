88d084fd07f9d500a7e534008553ae5b482ed8e9
Matthew Perrot <Matthew@Matthews-MacBook-Air.local>
2022-09-25 19:47:04 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,28 +1,85 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
 
+//Index
 router.get('/', (req, res) => {
-  res.render('places/index', { places })
-});
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
-});
-
-router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', { places })
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
+  })
 })
 
-module.exports = router;
+//NEW
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+//CREATE
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    //Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  db.Place.create(req.body)
+  .then(() => {
+    res.redirect('/places')
+  })
+  .catch(err => {
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for( var field in err.errors){
+        message += `${field} was ${err.errors[field].value}. `
+        message += `${err.errors [field].message}`
+      }
+      res.render('places/new', { message })
+    }
+    else{
+    res.render('error404')
+    }
+  })
+})  
+
+//Show Page
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+    res.render('places/show', { place })
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
+
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+//EDIT
+router.get('/:id/edit', (req, res) =>{
+  db.Place.findById(req.params.id)
+  .then(place => {
+    res.render('places/edit', { place })
+  })
+  .catch(err => {
+    res.render('error404')
+  })
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
